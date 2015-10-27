b12eabf07061158c37b34eee8557e51fb1b8ac84
Gary Allan <garyallan.net@gmail.com>
2015-10-27 17:04:23 +1100

    basic routing working

--- Swooshy/app/main.js
+++ Swooshy/app/main.js
@@ -1,12 +1,42 @@
-﻿(function () {
-    'use strict';
+﻿/**
+ * Created by garyallan on 10/10/2015.
+ */
+// create the module and name it azureApp
+var azureApp = angular.module('azureApp', ['ngRoute']);
 
-    angular
-        .module('app')
-        .controller('Main', main);
+// configure our routes
+azureApp.config(function ($routeProvider) {
+    $routeProvider
 
-    function main() {
-        var vm = this;
-        vm.food = 'Chinese food';
-    }
-})();
+        // route for the home page
+        .when('/', {
+            templateUrl: 'home.html',
+            controller: 'mainController'
+        })
+
+        // route for the about page
+        .when('/about', {
+            templateUrl: 'about.html',
+            controller: 'aboutController'
+        })
+
+        // route for the contact page
+        .when('/contact', {
+            templateUrl: 'contact.html',
+            controller: 'contactController'
+        });
+});
+
+// create the controller and inject Angular's $scope
+azureApp.controller('mainController', function ($scope) {
+    // create a message to display in our view
+    $scope.message = 'Angularjs making this possible!';
+});
+
+azureApp.controller('aboutController', function ($scope) {
+    $scope.message = 'The about page brought to you by Angularjs routing...';
+});
+
+azureApp.controller('contactController', function ($scope) {
+    $scope.message = 'Angularjs presenting the contact page...';
+});
